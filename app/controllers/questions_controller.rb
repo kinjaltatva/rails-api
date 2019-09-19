0d01f168b028b60b3d925735ab74f74f38bb620f
Kinjal <kinjal.thakkar@tatvasoft.com>
2019-09-19 19:48:28 +0530

    rails api

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+
+  before_action :get_question, only: [:update, :destroy]
+
+  def index
+    questions = Question.all
+    render json: questions.to_json( :include => [:mapping, :role] ), status: 200
+  end
+
+  def create
+    question = Question.create(question_params)
+    render json: question.to_json, status: 200
+  end
+
+  def update
+    question.update(question_params)
+    render json: question.to_json, status: 200
+  end
+
+  def destroy
+    question.destroy
+    render json: {message: 'Question is destroy successfully'}, status: 200
+  end
+
+  def mapping_list
+    mappings = Mapping.all
+    render json: mappings.to_json, status: 200
+  end
+
+  def role_list
+    roles = Role.all
+    render json: roles.to_json, status: 200
+  end
+
+
+  private
+
+  def get_question
+    question = Question.find_by(id: params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:role_id, :mapping_id, :question_type, :is_required, :team_stage, :appear, :conditions, :frequency)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :questions do
+    collection do
+      get :mapping_list
+      get :role_list
+    end
+  end
+end
